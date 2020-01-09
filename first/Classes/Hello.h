a03f35dc5fc277fef81f32ae9afa5fb0a9735327
yifuqiu <msicr400@163.com>
2020-01-09 23:28:00 +0800

    first commit

---
+++ first/Classes/Hello.h
@@ -0,0 +1,17 @@
+//
+//  Hello.h
+//  1217
+//
+//  Created by yfq on 2020/1/9.
+//  Copyright © 2020 yfq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Hello : NSObject
++ (void)helloworld;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ first/Classes/Hello.m
@@ -0,0 +1,15 @@
+//
+//  Hello.m
+//  1217
+//
+//  Created by yfq on 2020/1/9.
+//  Copyright © 2020 yfq. All rights reserved.
+//
+
+#import "Hello.h"
+
+@implementation Hello
++ (void)helloworld{
+    NSLog(@"helloworld----");
+}
+@end
